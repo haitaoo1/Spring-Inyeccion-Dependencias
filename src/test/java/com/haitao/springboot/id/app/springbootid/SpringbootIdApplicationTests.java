cf62f3a39e1e5773d0b5d8e1b333cc27d3cc2d38
Haitao Wu <haitaowu14@gmail.com>
2023-12-28 21:33:27 +0100

    Vacio

---
+++ src/test/java/com/haitao/springboot/id/app/springbootid/SpringbootIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitao.springboot.id.app.springbootid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
